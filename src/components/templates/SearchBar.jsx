41a15d584f622242316936ae29e18bebfa19f6ff
saddansyah <saddanakbar@gmail.com>
2023-05-05 20:36:07 +0700

    feat: add profile page

--- src/components/templates/SearchBar.jsx
+++ src/components/templates/SearchBar.jsx
@@ -5,13 +5,13 @@ import { InputBase, IconButton } from '@mui/material';
 const SearchBar = ({placeholder}) => {
     return (
         <>
-            <div className='flex items-center gap-3'>
+            <div className='flex items-center gap-1'>
                 <InputBase
                     id='search'
                     className='bg-gray-200 rounded-full hover:outline hover:outline-1 hover:outline-slate-400'
-                    sx={{ px: 2, py: 1 }}
+                    sx={{ px: 2  }}
                     placeholder={placeholder}
-                    size='small'
+                    size='large'
                     fullWidth
                     type='search'
                     autoFocus
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,101 @@
 import Menu from "@mui/icons-material/Menu";
-import { Avatar } from "@mui/material";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import EmailIcon from '@mui/icons-material/Email';
+import PhoneIcon from '@mui/icons-material/Phone';
+
+import { Avatar, ButtonBase, InputAdornment, TextField } from "@mui/material";
 import { Navbar } from "@/utils/componentsLoader";
 
 const Profile = () => {
+    const dummyUser = {
+        username: 'wakeguard',
+        name: 'Wakeguard User',
+        email: 'gmail@wakeguard.com',
+        phoneNumber: '081293812939'
+    }
     return (
         <>
             <Navbar>
                 <Avatar>SA</Avatar>
                 <Menu />
             </Navbar>
-            <h1 className="font-bold text-2xl text-primary mb-6">Profile</h1>
+            <h1 className="font-bold text-2xl text-primary mb-6">My Profile</h1>
+            <div className="profile-summary flex flex-row gap-8 items-start h-fit">
+                <Avatar src="https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2021/05/18/1067981407.jpg" sx={{ width: 120, height: 120, fontSize: 50 }}>SA</Avatar>
+                <div className="content-right">
+                    <div className="username card">
+                        <h2 className="text-2xl font-semibold">@{dummyUser.username}</h2>
+                    </div>
+                    <div className="edit-button mt-3"></div>
+                    <ButtonBase component="div">
+                        <div className="p-2 rounded-lg bg-accent text-white">
+                            <span><ModeEditIcon /></span> Edit Profile
+                        </div>
+                    </ButtonBase>
+                </div>
+            </div>
+            <div className="profile-info mt-12">
+                <h2 className="font-bold mb-2">Profile Information</h2>
+                <div className="card">
+                    <div className="mb-3">
+                        <TextField
+                            fullWidth
+                            disabled
+                            id="name"
+                            label="Name"
+                            value={dummyUser.name}
+                            variant="outlined"
+                            InputProps={{
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <AccountCircleIcon />
+                                    </InputAdornment>
+                                )
+                            }}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <TextField
+                            fullWidth
+                            disabled
+                            id="email"
+                            label="Email"
+                            value={dummyUser.email}
+                            variant="outlined"
+                            InputProps={{
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <EmailIcon />
+                                    </InputAdornment>
+                                )
+                            }}
+                        />
+                    </div>
+                    <div className="">
+                        <TextField
+                            fullWidth
+                            disabled
+                            id="phoneNumber"
+                            label="Phone Number"
+                            value={dummyUser.phoneNumber}
+                            variant="outlined"
+                            InputProps={{
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <PhoneIcon />
+                                    </InputAdornment>
+                                )
+                            }}
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className="logout mt-12 mb-20">
+                <ButtonBase component="button" className="w-full">
+                    <div className="card bg-red-500 text-white text-center font-semibold w-full">Log Out</div>
+                </ButtonBase>
+            </div>
         </>
     );
 }
