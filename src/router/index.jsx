5775e2b2fe57a1311e10f3c68b82ee257ef73309
saddansyah <saddanakbar@gmail.com>
2023-05-05 20:40:23 +0700

    chore: router indentation

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,53 +1,53 @@
 import {
-    createBrowserRouter,
-    createRoutesFromElements,
-    Route,
-    Navigate
-  } from "react-router-dom";
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Navigate
+} from "react-router-dom";
 
 // User-defined
 import {
-    App,
-    NotFound,
-    BaseLayout,
-    Login,
-    Register,
-    Home,
-    DrivingMode,
-    DrivingTips,
-    Emergency,
-    Profile
-  } from '@/utils/pagesLoader';
+  App,
+  NotFound,
+  BaseLayout,
+  Login,
+  Register,
+  Home,
+  DrivingMode,
+  DrivingTips,
+  Emergency,
+  Profile
+} from '@/utils/pagesLoader';
 
 import DynamicTitle from "@/utils/dynamicTitle";
 
-  const isLoggedIn = () => {
+const isLoggedIn = () => {
 
-    // Auth logic...
-    
-    return true;
-  }
-  
-  const router = createBrowserRouter(createRoutesFromElements(
-    <Route path='/' element={<BaseLayout/>}>
-      <Route exact path='/' element={isLoggedIn() ? <App/> : <Navigate to='auth/login' replace />}>
-        <Route path='/' element={<DynamicTitle title="Home"><Home/></DynamicTitle>} />
-        <Route path='/driving-mode' element={<DynamicTitle title="Driving Mode"><DrivingMode/></DynamicTitle>} />
-        <Route path='/emergency' element={<DynamicTitle title="Emergency"><Emergency/></DynamicTitle>} />
-        <Route path='/driving-tips' element={<DynamicTitle title="Driving Tips"><DrivingTips/></DynamicTitle>} />
-        <Route path='/profile' element={<DynamicTitle title="Profile"><Profile/></DynamicTitle>} />
-        <Route path='*' element={<DynamicTitle title="404 Not Found"><NotFound/></DynamicTitle>} />
-      </Route>
-      <Route exact path='auth' element={isLoggedIn() ? <Navigate to='/' replace/> : <Login/>}>
-        <Route path='login' element={<Login/>} />
-        <Route path='register' element={<Register/>} />
-      </Route>
-    </Route >
-  ))
+  // Auth logic...
 
-  export {
-    isLoggedIn,
-    router
-  }
+  return true;
+}
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<BaseLayout />}>
+    <Route exact path='/' element={isLoggedIn() ? <App /> : <Navigate to='auth/login' replace />}>
+      <Route path='/' element={<DynamicTitle title="Home"><Home /></DynamicTitle>} />
+      <Route path='/driving-mode' element={<DynamicTitle title="Driving Mode"><DrivingMode /></DynamicTitle>} />
+      <Route path='/emergency' element={<DynamicTitle title="Emergency"><Emergency /></DynamicTitle>} />
+      <Route path='/driving-tips' element={<DynamicTitle title="Driving Tips"><DrivingTips /></DynamicTitle>} />
+      <Route path='/profile' element={<DynamicTitle title="Profile"><Profile /></DynamicTitle>} />
+      <Route path='*' element={<DynamicTitle title="404 Not Found"><NotFound /></DynamicTitle>} />
+    </Route>
+    <Route exact path='auth' element={isLoggedIn() ? <Navigate to='/' replace /> : <Login />}>
+      <Route path='login' element={<Login />} />
+      <Route path='register' element={<Register />} />
+    </Route>
+  </Route >
+))
+
+export {
+  isLoggedIn,
+  router
+}
 
 
